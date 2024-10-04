805865be3ca8f1cf0f27bb205032364f9accaf6e
Zach Radford <zradford@hubspot.com>
2024-10-04 16:07:45 -0400

    seed data for native associations created

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,202 +1,104 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, AssociationType } from "@prisma/client"; // Import the AssociationType enum
 import handleError from '../src/utils/error';
 
-//Should be refactored to a single PrismaClient instance
 const prisma = new PrismaClient({
   log: ['info', 'warn', 'error'],
 });
-async function main(): Promise<void> {
-  const firstname = await prisma.properties.upsert({
-    where: {
-      name_object_customerId: {
-        name: "firstname",
-        object: "Contact",
-        customerId: "1",
-      },
-    },
-    update: {},
-    create: {
-      name: "firstname",
-      label: "First Name",
-      type: "String",
-      object: "Contact",
-      customerId: "1",
-      modificationMetadata:{
-      archivable: true,
-      readOnlyDefinition: false,
-      readOnlyValue: false,
-      }
 
+async function main() {
+  const associationsData = [
+    {
+      nativeObject: "Contact",
+      toNativeObject: "Contract",
+      nativeAssociationLabel: "Contact to Contract",
+      associationType: AssociationType.ONE_TO_ONE,
+      customerId: "1",
     },
-  });
-  console.log(firstname);
+    {
+      nativeObject: "Company",
+      toNativeObject: "Branch",
+      nativeAssociationLabel: "Company to Branch",
+      associationType: AssociationType.ONE_TO_MANY,
+      customerId: "1",
+    },
+    {
+      nativeObject: "Employee",
+      toNativeObject: "Department",
+      nativeAssociationLabel: "Employee to Department",
+      associationType: AssociationType.ONE_TO_MANY,
+      customerId: "1",
+    },
+    {
+      nativeObject: "Student",
+      toNativeObject: "Course",
+      nativeAssociationLabel: "Student to Course",
+      associationType: AssociationType.MANY_TO_MANY,
+      customerId: "1",
+    },
+    {
+      nativeObject: "Vendor",
+      toNativeObject: "Product",
+      nativeAssociationLabel: "Vendor to Product",
+      associationType: AssociationType.MANY_TO_MANY,
+      customerId: "1",
+    },
+    {
+      nativeObject: "Contact",
+      toNativeObject: "Doctor",
+      nativeAssociationLabel: "Patient to Doctor",
+      associationType: AssociationType.ONE_TO_ONE,
+      customerId: "1",
+    },
+    {
+      nativeObject: "Contact",
+      toNativeObject: "Article",
+      nativeAssociationLabel: "Writer to Article",
+      associationType: AssociationType.ONE_TO_MANY,
+      customerId: "1",
+    },
+    {
+      nativeObject: "Contact",
+      toNativeObject: "Gallery",
+      nativeAssociationLabel: "Artist to Gallery",
+      associationType: AssociationType.MANY_TO_MANY,
+      customerId: "1",
+    }
+  ];
 
-  const lastname = await prisma.properties.upsert({
-    where: {
-      name_object_customerId: {
-        name: "lastname",
-        object: "Contact",
-        customerId: "1",
-      },
-    },
-    update: {},
-    create: {
-      name: "lastname",
-      label: "Last Name",
-      type: "String",
-      object: "Contact",
-      customerId: "1",
-      modificationMetadata:{
-        archivable: true,
-        readOnlyDefinition: false,
-        readOnlyValue: false,
-        }
-    },
-  });
-  console.log(lastname);
-  const exampleCustom = await prisma.properties.upsert({
-    where: {
-      name_object_customerId: {
-        name: "example_custom",
-        object: "Contact",
-        customerId: "1",
-      },
-    },
-    update: {},
-    create: {
-      name: "example_custom",
-      label: "Example Custom Property",
-      type: "String",
-      object: "Contact",
-      customerId: "1",
-      modificationMetadata:{
-        archivable: true,
-        readOnlyDefinition: false,
-        readOnlyValue: false,
-        }
-    },
-  });
-  console.log(exampleCustom);
-  const exampleRequired = await prisma.properties.upsert({
-    where: {
-      name_object_customerId: {
-        name: "example_required",
-        object: "Contact",
-        customerId: "1",
-      },
-    },
-    update: {},
-    create: {
-      name: "example_required",
-      label: "Example Required Property",
-      type: "String",
-      object: "Contact",
-      customerId: "1",
-      modificationMetadata:{
-        archivable: true,
-        readOnlyDefinition: false,
-        readOnlyValue: true,
-        }
-    },
-  });
-  console.log(exampleRequired);
-  const customCompany = await prisma.properties.upsert({
-    where: {
-      name_object_customerId: {
-        name: "example_custom_company",
-        object: "Company",
-        customerId: "1",
-      },
-    },
-    update: {},
-    create: {
-      name: "example_custom_company",
-      label: "Example Custom Property",
-      type: "String",
-      object: "Company",
-      customerId: "1",
-      modificationMetadata:{
-        archivable: true,
-        readOnlyDefinition: false,
-        readOnlyValue: false,
-        }
-    },
-  });
-  console.log(customCompany);
-  const companyName = await prisma.properties.upsert({
-    where: {
-      name_object_customerId: {
-        name: "company_name",
-        object: "Company",
-        customerId: "1",
-      },
-    },
-    update: {},
-    create: {
-      name: "company_name",
-      label: "Name",
-      type: "String",
-      object: "Company",
-      customerId: "1",
-      modificationMetadata:{
-        archivable: true,
-        readOnlyDefinition: false,
-        readOnlyValue: false,
-        }
-    },
-  });
-  console.log(companyName);
-  const industry = await prisma.properties.upsert({
-    where: {
-      name_object_customerId: {
-        name: "industry",
-        object: "Company",
-        customerId: "1",
-      },
-    },
-    update: {},
-    create: {
-      name: "industry",
-      label: "Industry",
-      type: "String",
-      object: "Company",
-      customerId: "1",
-      modificationMetadata:{
-        archivable: true,
-        readOnlyDefinition: false,
-        readOnlyValue: false,
-        }
-    },
-  });
-  console.log(industry);
-  const num_employees = await prisma.properties.upsert({
-    where: {
-      name_object_customerId: {
-        name: "num_employees",
-        object: "Company",
-        customerId: "1",
-      },
-    },
-    update: {},
-    create: {
-      name: "num_employees",
-      label: "Number of Employees",
-      type: "Number",
-      object: "Company",
-      customerId: "1",
-      modificationMetadata:{
-        archivable: true,
-        readOnlyDefinition: false,
-        readOnlyValue: false,
-        }
-    },
-  });
-  console.log(num_employees);
+  try {
+    for (const association of associationsData) {
+      const result = await prisma.association.upsert({
+        where: {
+          customerId_nativeObject_toNativeObject_nativeAssociationLabel: {
+            customerId: association.customerId,
+            nativeObject: association.nativeObject,
+            toNativeObject: association.toNativeObject,
+            nativeAssociationLabel: association.nativeAssociationLabel,
+          },
+        },
+        update: {},
+        create: association,
+      });
+      console.log("Upserted:", result);
+    }
+  } catch (error: any) {
+    handleError('Something went wrong while seeding the database', error);
+  }
+
+  // Fetch and log all associations
+  const allAssociations = await prisma.association.findMany();
+  console.log("All Associations:");
+  allAssociations.forEach((association) => console.log(association));
 }
+
 
 main()
   .catch((e) => {
-    handleError(e, 'There was an issue seeding the database ', true)
+    console.error("Error seeding database", e);
+    process.exit(1);
   })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
-  export default prisma
+export default prisma;
